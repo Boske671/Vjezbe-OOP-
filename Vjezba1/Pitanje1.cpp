1d793f7480dbd2c193831fa7d9cb230e79e788ac
Boske671 <164241422+Boske671@users.noreply.github.com>
2024-03-20 20:48:31 +0100

    Add files via upload

---
+++ Vjezba1/Pitanje1.cpp
@@ -0,0 +1,162 @@
+﻿//Vlasnik ste niza plaža na najljepšoj plaži u Pučišću na Braču.Na plažama iznamljujete razne predmete kao što su suncobrani, lopte, stolci i slično, te vam je već pomalo nespretno voditi evidenciju o tome koje sve plaže imate, a pogotovo koji su sve predmeti dostupni na svakoj te ste se odlučili baciti na posao i napraviti C++ program koji će vam u tome pomoći.
+//
+//Odlučili ste definirati niz plaža, i za svaku plažu bilježiti niz predmeta koje iznamljujete(kako vam ne bi nestali).
+//
+//Klasa Plaza ima sljedeća svojstva :
+//
+//veličinu(duljina)
+//GPS koordinate(double X i double Y)
+//niz predmeta koji su dostupni
+//
+//Klasa Predmet ima sljedeća svojstva :
+//
+//opis
+//kolicina
+//
+//U main programu potrebno je od korisnika tražiti unos broja N, nakon kojeg se unose podaci o N plaža.Nakon što se unesu sva polja, potrebno je tražiti unos broja M, koji označava broj predmeta, te je potrebno unijeti tih M predmeta(neka plaža može nuditi više predmeta).Kod unosa pripaziti da se GPS lokacija doista odnosi na mjesto u blizini Pučišća.
+//
+//Primjer izvođenja :
+//
+//Unesite broj plaža(N) : 2
+//Unesite velicinu i lokaciju za 1. plazu :
+//	1345
+//	43.346216 16.734324
+//	Unesite velicinu i lokaciju za 2. plazu :
+//	2115
+//	43.348236 16.734334
+//	Unesite broj predmeta(M) : 1
+//	Unesite redni broj plaze kojoj pripada 1. predmet : 2
+//	Unesite kolicinu i opis predmeta : 19 Lopta
+//
+//	Ispis plaza i predmeta :
+//1. 43.346216 16.734324 D = 1345 - predmeti :
+//
+//	2. 43.348236 16.734334 D = 2115 - predmeti : 19 Lopta
+
+
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <cmath>
+
+
+/*
+UPIS
+2
+1345
+43.346216 16.734324
+2115
+43.348236 16.734334
+1
+2
+19 Lopta 
+*/
+using namespace std;
+class Predmet
+{
+public:
+	string opis;
+	int kolicina;
+	Predmet(int _kolicina, string _opis)
+	{
+		kolicina = _kolicina;
+		opis = _opis;
+	}
+};
+
+class Plaza
+{
+public:
+	int velicina;
+	double x, y;
+	vector <Predmet> predmeti;
+	Plaza(int _velicina, double _x, double _y)
+	{
+		velicina = _velicina;
+		x = _x;
+		y = _y;
+	}
+
+	//Kod unosa pripaziti da se GPS lokacija doista odnosi na mjesto u blizini Pučišća. => 43.34778 16.73389
+	bool Provjerablizine(double unesena_x, double unesena_y)
+	{
+		const double max_dop = 0.1;
+
+		//KOORDINATE Pučišća
+		const double x_p = 43.34778;
+		const double y_p = 16.73389;
+
+		//EUKLIDSKA FORMULA
+		double udaljenost = pow(pow(x_p - unesena_x, 2) + pow(y_p - unesena_y, 2), 0.5);
+
+		//TRUE AKO JE UDALJENOST MANJA OD MAKSIMALNE DOPUSTENE
+		return udaljenost <= max_dop;
+	}
+
+	void DodavanjePredmeta (const Predmet& p)
+	{
+		predmeti.push_back(p);
+	}
+	
+	// Ispisuje informacije o plaži i predmetima
+	void ispisiPlazu() {
+		cout << x << " " << y << " D= " << velicina << "- predmeti: ";
+		for (Predmet predmet : predmeti) {
+			cout << predmet.kolicina << " " << predmet.opis << " ";
+		}
+		cout << endl;
+	}
+};
+
+int main()
+{
+	
+	int n;
+	vector<Plaza> plaze;
+
+	cout << "Unesite broj plaza (N):";
+	cin >> n;
+	
+
+	int vel;
+	double x, y;
+	for (int i = 0; i < n; i++)
+	{
+		cout << "Unesite velicinu i lokaciju za " << i + 1 << ". plazu:" << endl;
+		cin >> vel;
+
+		//UPIS I PROVJERA KORDINATA
+		do
+		{
+			cin >> x >> y;
+			if (Plaza(vel, x, y).Provjerablizine(x, y) == 0)
+			{
+				cout << "Unesi ispravne koordinate za plazu!" << endl;
+			}
+		} while (Plaza(vel, x, y).Provjerablizine(x, y) == 0);
+		plaze.push_back(Plaza(vel, x, y));
+	}
+
+	int m, r, kol;
+	string opis;
+	cout << "Unesite broj predmeta (M): ";
+	cin >> m;
+
+	for (int i = 0; i < m; i++)
+	{
+		cout << "Unesite redni broj plaze kojoj pripada " << i + 1 << ". predmet: ";
+		cin >> r;
+		cout << "Unesite kolicinu i opis predmeta: ";
+		cin >> kol >> opis;
+		plaze[r - 1].DodavanjePredmeta(Predmet(kol, opis));
+	}
+
+	cout << "Ispis plaza i predmeta:" << endl;
+	for (int i = 0; i < n; i++) {
+		cout << i + 1 << ". ";
+		plaze[i].ispisiPlazu();
+	}
+}
+
---
+++ Vjezba1/Pitanje2.cpp
@@ -0,0 +1,157 @@
+﻿/*
+Potrebno je napisati program za evidenciju igrača u ekipi. Ekipa se sastoji od igrača, a za svakog igrača se evidentira broj na dresu, ime, prezime i trzisna cijena. Potrebno je omogućiti ispis svih igrača u ekipi (naziv ekipe, te popis svih igrača poredano po broju na dresu). Također, potrebno je omogućiti jednostavno dohvaćanje informacija u ukupnoj cijeni svih igrača, najskupljem igraču, te prosječnoj cijeni svih igrača. Slijedeći programski odsječak u nastavku napišite sve potrebne klase i metode kojima se dobije očekivano ponašanje programskog koda.
+int main(){
+
+       Ekipa ekipa;
+
+       ekipa.naziv = "Tim raketa";
+
+       Igrac i1("Luka", "Modric");
+
+       i1.broj = 10;
+
+       i1.cijena = 50000000;
+
+       ekipa.dodajIgraca(i1);
+
+       Igrac i2("Mario", "Mandzukic");
+
+       i2.broj = 9;
+
+       i2.cijena = 27000000;
+
+       ekipa.dodajIgraca(i2);
+
+       Igrac i3("Darijo", "Srna");
+
+       i3.broj = 11;
+
+       i3.cijena = 14500000;
+
+       ekipa.dodajIgraca(i3);
+
+ 
+
+       ekipa.ispisi();
+
+ 
+
+       Igrac najskuplji = ekipa.dohvatiNajskupljeg();
+
+       cout << "Najskuplji igrac: ";
+
+       najskuplji.ispisi();
+
+ 
+
+       cout << "Ukupna cijena svih igraca: " << ekipa.ukupnaCijena() << endl;
+
+       cout << "Prosjecna cijena jednog igraca: " << ekipa.prosjecnaCijena() << endl;
+
+}
+*/
+
+
+
+
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+class Igrac
+{
+public:
+    int broj, cijena;
+    string ime, prezime;
+
+    Igrac(string _ime, string _prezime)
+    {
+        ime = _ime;
+        prezime = _prezime;
+    }
+
+    void ispisi() {
+        cout << "Ime: " << ime << " - Prezime: " << prezime << " - Broj na dresu: " << broj << " - Cijena: " << cijena << endl;
+    }
+};
+
+
+class Ekipa
+{
+public:
+    string naziv;
+    vector<Igrac> igraci;
+
+    void dodajIgraca(Igrac ig)
+    {
+        igraci.push_back(ig);
+    }
+
+    void ispisi()
+    {
+        cout << "Naziv ekipe: " << naziv << endl;
+        cout << "Popis igraca:" << endl;
+        for (Igrac ig : igraci)
+        {
+            ig.ispisi();
+        }
+    }
+
+
+    Igrac dohvatiNajskupljeg()
+    {
+        Igrac najskuplji = igraci[0];
+        for (Igrac ig : igraci)
+        {
+            if (ig.cijena > najskuplji.cijena) 
+            {
+                najskuplji = ig; 
+            }
+        }
+        return najskuplji;
+    }
+
+    int ukupnaCijena()
+    {
+        int uk = 0;
+        for (Igrac ig : igraci)
+        {
+            uk += ig.cijena;
+        }
+        return uk;
+    }
+
+    int prosjecnaCijena()
+    {
+        double pros = (double)ukupnaCijena() / igraci.size();
+        return pros;
+    }
+};
+
+int main()
+{
+    Ekipa ekipa;
+    ekipa.naziv = "Tim raketa";
+    Igrac i1("Luka", "Modric");
+    i1.broj = 10;
+    i1.cijena = 50000000;
+    ekipa.dodajIgraca(i1);
+    Igrac i2("Mario", "Mandzukic");
+    i2.broj = 9;
+    i2.cijena = 27000000;
+    ekipa.dodajIgraca(i2);
+    Igrac i3("Darijo", "Srna");
+    i3.broj = 11;
+    i3.cijena = 14500000;
+    ekipa.dodajIgraca(i3);
+    ekipa.ispisi();
+    Igrac najskuplji = ekipa.dohvatiNajskupljeg();
+    cout << "Najskuplji igrac: ";
+    najskuplji.ispisi();
+    cout << "Ukupna cijena svih igraca: " << ekipa.ukupnaCijena() << endl;
+    cout << "Prosjecna cijena jednog igraca: " << ekipa.prosjecnaCijena() << endl;
+}
+
